97e2a34b6fa01fbbe80b03291662ec0f7dbc530a
AaronStearns <stearnsconcepts@gmail.com>
2019-09-26 12:13:58 -0400

    Additional code cleanup and comment removal

--- src/App.vue
+++ src/App.vue
@@ -61,55 +61,32 @@ export default {
     methods: {
         rollDie() {
             var roll = Math.floor(Math.random() * 6) + 1;
-            this.diceRolls.push(roll)
 
-            var reduceToKeyVals = this.diceRolls.reduce((acc, val) => {
+            this.diceRolls.push(roll);
+
+            var reduceArrayToKeyValPairs = this.diceRolls.reduce((acc, val) => {
                 acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
                 return this.diceRollKeyVals = acc;
             }, {});
+            
             this.populateTags();
 
         },
         checkForWinner(key, val) {
             if (val == 5) {
-                alert("Winner! Side " + key)
+                alert("Winner! Side " + key);
                 resetBoard();
             }
-        },
-        resetBoard() {
-                this.diceRolls = [];
-                this.diceRollKeyVals = {};
-
-                var one = document.getElementById("1");
-                var two = document.getElementById("2");
-                var three = document.getElementById("3");       
-                var four = document.getElementById("4");
-                var five = document.getElementById("5");
-                var six = document.getElementById("6");
-
-                one.innerText = "";
-                two.innerText = "";
-                three.innerText = "";
-                four.innerText = "";
-                five.innerText = "";
-                six.innerText = "";
-
-                one.parentElement.querySelector("div").setAttribute("style", "height: 0%");
-                two.parentElement.querySelector("div").setAttribute("style", "height: 0%");
-                three.parentElement.querySelector("div").setAttribute("style", "height: 0%");
-                four.parentElement.querySelector("div").setAttribute("style", "height: 0%");
-                five.parentElement.querySelector("div").setAttribute("style", "height: 0%");
-                six.parentElement.querySelector("div").setAttribute("style", "height: 0%");
         },
         populateTags() {
             for (const [key, value] of Object.entries(this.diceRollKeyVals)) {
 
-                var one = document.getElementById("1")
-                var two = document.getElementById("2")
-                var three = document.getElementById("3")
-                var four = document.getElementById("4")
-                var five = document.getElementById("5")
-                var six = document.getElementById("6")
+                var one = document.getElementById("1");
+                var two = document.getElementById("2");
+                var three = document.getElementById("3");
+                var four = document.getElementById("4");
+                var five = document.getElementById("5");
+                var six = document.getElementById("6");
         
                 if (key == "1") {
                     one.innerText = value;
@@ -117,72 +94,88 @@ export default {
                     one.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: pink; height: " + height + "%");
-                    this.checkForWinner(key, value)                 
+                    this.checkForWinner(key, value)         ;        
                 } else if (key == "2") {
                     two.innerHTML = value;
                     var height = value * 20;
                     two.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: peachpuff; height: " + height + "%");
-                    this.checkForWinner(key, value)
+                    this.checkForWinner(key, value);
                 } else if (key == "3") {
                     three.innerHTML = value;
                     var height = value * 20;
                     three.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: Khaki ; height: " + height + "%");
-                    this.checkForWinner(key, value)
+                    this.checkForWinner(key, value);
                 } else if (key == "4") {
                     four.innerHTML = value;
                     var height = value * 20;
                     four.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: palegreen; height: " + height + "%");
-                    this.checkForWinner(key, value)
+                    this.checkForWinner(key, value);
                 } else if (key == "5") {
                     five.innerHTML = value;
                     var height = value * 20;
                     five.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: paleturquoise; height: " + height + "%");
-                    this.checkForWinner(key, value)
+                    this.checkForWinner(key, value);
                 } else if (key == "6") {
                     six.innerHTML = value;
                     var height = value * 20;
                     six.parentElement
                         .querySelector("div")
                         .setAttribute("style", "background-color: lightsteelblue; height: " + height + "%");
-                    this.checkForWinner(key, value)
+                    this.checkForWinner(key, value);
                 }
             }
+        },        
+        resetBoard() {
+            this.diceRolls = [];
+            this.diceRollKeyVals = {};
+
+            var one = document.getElementById("1");
+            var two = document.getElementById("2");
+            var three = document.getElementById("3");       
+            var four = document.getElementById("4");
+            var five = document.getElementById("5");
+            var six = document.getElementById("6");
+
+            one.innerText = "";
+            two.innerText = "";
+            three.innerText = "";
+            four.innerText = "";
+            five.innerText = "";
+            six.innerText = "";
+
+            one.parentElement.querySelector("div").setAttribute("style", "height: 0%");
+            two.parentElement.querySelector("div").setAttribute("style", "height: 0%");
+            three.parentElement.querySelector("div").setAttribute("style", "height: 0%");
+            four.parentElement.querySelector("div").setAttribute("style", "height: 0%");
+            five.parentElement.querySelector("div").setAttribute("style", "height: 0%");
+            six.parentElement.querySelector("div").setAttribute("style", "height: 0%");
         }
     }
 }
 </script>
 
 <style scoped>
-
-#app {
-    /* background-color:  FloralWhite; */
-}
-
 .button {
     width: 150px;
     height: 60px;
     border: 1px solid darkgrey;    
-    /* justify-content: center; */
-  margin:auto;
-  display:block;
-  margin-bottom: 10px;
+      margin:auto;
+    display:block;
+    margin-bottom: 10px;
 }
 
 #resetButton {
-    /* margin-top: 150px; */
     width: 120px;
     height: 40px;
     border: 1px solid darkgrey;    
-    /* background-color: rgb(248, 148, 165); */
-    /* justify-content: center; */
     margin:auto;
     display:block;
 }
@@ -193,24 +186,16 @@ export default {
     display: flex;
     width: 200px;
     height: 200px;
-    /* margin-top: 300px; */
     border: 1px solid black;
-    /* vertical-align: middle; */
-    /* margin-top: 200px; */
     position: relative;
     background-color: WhiteSmoke;
     margin: 20px;
 }
 
-
 .inlineDice img {
     padding-left: 10px;
     text-align: center;
-    /* padding-top: 20px; */
     width: 80px;
-    /* justify-content: center;
-    align-items: center; */
-    /* display: flex; */
 }
 
 .inlineDice p {
@@ -226,24 +211,6 @@ export default {
     bottom: 0; 
     right: 0;
     margin-right: 15px;
-      /* border: 1px solid black; */
     width: 40px;
-    /* position: absolute; */
 }
-
-/* #content div img {
-    padding-top: 0;
-
-}
-
-#content div div {
-    padding: 0;
-    margin: 0;
-}
-#content div {
-    display: inline-block;
-    width: 200px;
-    height: 100px;
-} */
-
 </style>
